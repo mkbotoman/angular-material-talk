2aab357fbc83ca4a772087350398ffae65a03ebd
Maureen <mkbotoman@gmail.com>
2015-09-07 23:10:05 -0600

    adding services and directives

--- README.md
+++ README.md
@@ -29,6 +29,11 @@
 8. Open <http://localhost:8000> to view your presentation
 
    You can change the port by using `grunt serve --port 8001`.
+
+## Reveal.js
+
+This slide deck uses reveal.js. Check it out here: https://github.com/hakimel/reveal.js/
+
 ## License
 
 MIT licensed
--- js/material-controller.js
+++ js/material-controller.js
@@ -1,8 +1,48 @@
 var app = angular.module('SlidesApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$mdBottomSheet', '$mdDialog', '$mdToast', function($scope, $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast){
+
+  $scope.project = {
+    description: ''
+  };
+  $scope.data = {};
+  $scope.data.cb1 = true;
+
+  $scope.data = {
+    group1 : 'Banana',
+    group2 : '2',
+    group3 : 'avatar-1'
   };
 
+  $scope.determinateValue = 30;
+  $scope.determinateValue2 = 30;
+
+  $scope.openLeftMenu = function() {
+    $mdSidenav('left').toggle();
+  };
+
+  $scope.openBottomSheet = function() {
+    $mdBottomSheet.show({
+      template: '<md-bottom-sheet style="height:50px; background:#3F51B5; font-size:40px; color:white;">Hello!</md-bottom-sheet>'
+  })};
+  var alert;
+  $scope.showAlert = showAlert;
+
+  function showAlert() {
+      alert = $mdDialog.alert({
+        title: 'Attention',
+        content: 'This is an example of how easy dialogs can be!',
+        ok: 'Close'
+      });
+      $mdDialog
+        .show( alert )
+        .finally(function() {
+          alert = undefined;
+        });
+    }
+    
+    $scope.openToast = function($event) {
+      $mdToast.show($mdToast.simple().content('Hello!'));
+      // Could also do $mdToast.showSimple('Hello');
+    };
 }]);
